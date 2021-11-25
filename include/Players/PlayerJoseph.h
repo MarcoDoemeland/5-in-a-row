fe547040a9f5b718d8e30e56afe4d945a68941cb
Mandrosys Mandrosys <joseph.lemainque@gmail.com>
2021-11-25 16:31:46 +0100

    Started to write the class and adapted it to the PlayerBase

--- include/Players/PlayerJoseph.h
+++ include/Players/PlayerJoseph.h
@@ -1,16 +1,29 @@
 #ifndef PLAYERJOSEPH_H
 #define PLAYERJOSEPH_H
 
+// Custom classes and defines
+#include "Defines.h"
+#include "PlayerBase.h"
+#include "Board.h"
+
+namespace FIAR{
+
 // Dumm, duemmer, am duemmsten
-
-class PlayerJoseph final{
+class PlayerJoseph final : public PlayerBase{
 public:
+    // Constructor
+    PlayerJoseph(Board* board);
+    // Destructor
+    virtual ~PlayerJoseph();
 
-
+    // Function to use to make an action
+    void doAction() override;
 
 private:
 
 
 };
+
+}// ENd namespace FIAR
 
 #endif // PLAYERJOSEPH_H
--- src/Players/PlayerJoseph.cpp
+++ src/Players/PlayerJoseph.cpp
@@ -1,3 +1,21 @@
 #include "PlayerJoseph.h"
 
+namespace FIAR{
 
+// Constructor
+PlayerJoseph::PlayerJoseph(Board* board)
+    : PlayerBase( board, "JosephBot" ){
+
+}
+
+// Destructor
+PlayerJoseph::~PlayerJoseph(){
+
+}
+
+// Function to use to make an action
+void PlayerJoseph::doAction(){
+
+}
+
+}// ENd namespace FIAR
