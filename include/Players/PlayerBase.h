daded196a2bba2e400b8f64026a47ec664134da7
Mandrosys Mandrosys <joseph.lemainque@gmail.com>
2021-11-25 16:27:59 +0100

    Modified the base players to make inheritance safer, added characteristics and pointer to the board as protected member

--- include/Players/PlayerBase.h
+++ include/Players/PlayerBase.h
@@ -5,58 +5,97 @@
 // ...
 //
 
-
-
-
 #ifndef __FIAR_PLAYERBASE_H__
 #define __FIAR_PLAYERBASE_H__
 
+// Standard library headers
+#include <string>
+#include <iostream>
+//#include <vector>
 
-#include <vector>
+// Custom classes and defines
+#include "Defines.h"
+#include "Board.h"
 
 // uhSchauMichAnIchBinDerMeisterDesCamelCase
 
-namespace FIAR
+namespace FIAR{
+
+// Abstract class, should be inherited
+class PlayerBase
 {
-	class PlayerBase
-	{
-		public:
-			// standard constructor
-			PlayerBase() {};
+public:
+    // Constructor
+    PlayerBase(Board* board, const std::string& playerName);
 
-			// constructor
-			PlayerBase( int i );
+    // constructor - to be removed? TODO
+    //~ PlayerBase( int i = 0 );
 
-			// constructor for square board
-			//~ Board( unsigned int const size );
+    // Destructor
+    virtual ~PlayerBase();
 
-			// destructor
-			//~ ~Printer( void );
+    // Asking the player to make an action
+    // This function is pure virtual, because doing an action should be specific to any type of player
+    virtual void doAction() = 0;
 
-			// ...
-			//~ auto add_symbol(	int const pos_x,
-								//~ int const pos_y,
-								//~ char const symbol
-							//~ ) -> bool;
+    // Accessor to the name and count of wins
+    std::string name() const;
+    int winCount() const;
 
-			// ...
-			//~ auto get_array( void ) -> std::vector<std::vector<char>>;
+    // Incrementing the count of wins
+    void incrementWinCount();
 
-			// ...
-			//~ auto find_winning_sequences( void ) -> std::vector< std::vector< int > >;
+protected:
+    // Pointer to the playboard
+    Board* m_board{ nullptr };
+    // Name of the player
+    std::string m_name{ "" };
+    // Count of wins
+    int m_winCount{ 0 };
 
-		private:
+private:
+    // Blocking the standard constructor.
+    // A base player will always need a board.
+    PlayerBase() = delete;
 
-			// size of board
-			//~ FIAR::Board board;
-			//~ unsigned int size_y;
-			//~ char default_symbol;
+    // Blocking copy constructor and copy assignement.
+    // Who the hell should be allowed to copy a base player after all?
+    PlayerBase(const PlayerBase& p) = delete;
+    PlayerBase& operator=(const PlayerBase& p) = delete;
 
-			// actual board representation
-			//~ std::vector<std::vector<char>> array;
-			//~ std::vector<char> array;
-	};
-}
+        // constructor for square board
+        //~ Board( unsigned int const size );
 
+        // destructor
+        //~ ~Printer( void );
+
+        // ...
+        //~ auto add_symbol(	int const pos_x,
+                            //~ int const pos_y,
+                            //~ char const symbol
+                        //~ ) -> bool;
+
+        // ...
+        //~ auto get_array( void ) -> std::vector<std::vector<char>>;
+
+        // ...
+        //~ auto find_winning_sequences( void ) -> std::vector< std::vector< int > >;
+
+    private:
+
+        // size of board
+        //~ FIAR::Board board;
+        //~ unsigned int size_y;
+        //~ char default_symbol;
+
+        // actual board representation
+        //~ std::vector<std::vector<char>> array;
+        //~ std::vector<char> array;
+};
+
+// Printing a player
+std::ostream& operator<<(std::ostream& stream, const PlayerBase& player);
+
+}// End namespace FIAR
 
 #endif
--- src/Players/PlayerBase.cpp
+++ src/Players/PlayerBase.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 
-#include "Players/PlayerBase.h"
+#include "PlayerBase.h"
 //~ #include "Board.h"
 
 namespace FIAR
 {
-	PlayerBase::PlayerBase( int i )
-	{
-		std::cout << i << '\n';
-	}
-
-
-
+// Constructor
+PlayerBase::PlayerBase(Board* board, const std::string& playerName)
+    : m_board{ board }
+    , m_name{ playerName }{
+    std::cout << "Constructing player " << *this << '\n';
 }
+
+// constructor - to be removed? TODO
+//~ PlayerBase::PlayerBase( int i ){
+//~     std::cout << i << '\n';
+//~ }
+
+// Destructor
+PlayerBase::~PlayerBase(){
+    std::cout << "Destroying player " << *this << '\n';
+}
+
+// Accessor to the name and count of wins
+std::string PlayerBase::name() const{
+    return m_name;
+}
+int PlayerBase::winCount() const{
+    return m_winCount;
+}
+
+// Incrementing the count of wins
+void PlayerBase::incrementWinCount(){
+    ++m_winCount;
+}
+
+// Printing the player
+std::ostream& operator<<(std::ostream& stream, const PlayerBase& player){
+    stream << player.name() << " (" << player.winCount() << " wins)";
+    return stream;
+}
+
+}// End namespace FIAR
