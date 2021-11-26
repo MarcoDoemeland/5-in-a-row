ec6cc379dda8ab6029f6fab28dda69519791d320
Mandrosys Mandrosys <joseph.lemainque@gmail.com>
2021-11-26 12:03:00 +0100

    The print function was moved to Board's code, using overloaded insertion operator. Two layouts are available, MarcoPrint and JosephPrint, each living in a seperate namespace for more clarity

--- include/Board.h
+++ include/Board.h
@@ -14,54 +14,83 @@
 
 #include "Defines.h"
 
-namespace FIAR
+namespace FIAR{
+
+using board_t = std::vector<std::vector<char>>;
+class Board
 {
-	class Board
-    {
-    public:
-        // standard constructor
-        Board() = default;
+public:
+    // standard constructor
+    Board() = default;
 
-        // constructor for square board
-        Board(std::size_t sizeX, std::size_t sizeY, char defaultSymbol = g_defaultSymbol);
+    // constructor for square board
+    Board(std::size_t sizeX, std::size_t sizeY, char defaultSymbol = g_defaultSymbol);
 
-        // No copy constructor
-        Board(const Board& board) = delete;
-        // No assignement operator
-        Board& operator=(const Board& b) = delete;
+    // No copy constructor
+    Board(const Board& board) = delete;
+    // No assignement operator
+    Board& operator=(const Board& b) = delete;
 
 
-        // constructor for square board
-        //~ Board( unsigned int const size );
+    // constructor for square board
+    //~ Board( unsigned int const size );
 
-        // destructor
-        //~ ~Printer( void );
+    // destructor
+    //~ ~Printer( void );
 
-        // ...
-        auto add_symbol(std::size_t posX, std::size_t posY, char symbol) -> bool;
+    // ...
+    auto add_symbol(std::size_t posX, std::size_t posY, char symbol) -> bool;
 
-        // ...
-        auto get_array( void ) -> std::vector<std::vector<char>>;// TODO: Probably pretty expensive
+    // ...
+    auto get_array( void ) -> std::vector<std::vector<char>>;// TODO: Probably pretty expensive
 
-        // ...
-        //~ auto find_winning_sequences( void ) -> std::vector< std::vector< int > >;
-        auto find_winning_sequences( void ) -> std::vector<WinningSequence>;
+    // ...
+    //~ auto find_winning_sequences( void ) -> std::vector< std::vector< int > >;
+    auto find_winning_sequences( void ) -> std::vector<WinningSequence>;
 
-    protected:
-        // Building the board, taking care of the dimensions
-        void buildBoard();
+    // Telling whether the board is valid
+    bool isValid() const;
+    // Returning the number of slots (the area) of the board
+    std::size_t area() const;
 
-    private:
-        // size of board
-        std::size_t m_sizeX{ g_boardSizeX };
-        std::size_t m_sizeY{ g_boardSizeY };
-        // Char to be displayed at each position by default
-        char m_defaultSymbol{ g_defaultSymbol };// The global variable should be enough, right?
+    // Overloading the << insertion operator
+    friend std::ostream& operator<<(std::ostream& stream, const Board& board);
 
-        // actual board representation
-        std::vector<std::vector<char>> m_array{ };// Maybe the previous one was better?
-        //~ std::vector<char> array;
-	};
+protected:
+    // Building the board, taking care of the dimensions
+    void buildBoard();
+
+private:
+    // size of board
+    std::size_t m_sizeX{ g_boardSizeX };
+    std::size_t m_sizeY{ g_boardSizeY };
+    // Char to be displayed at each position by default
+    char m_defaultSymbol{ g_defaultSymbol };// The global variable should be enough, right?
+
+    // actual board representation
+    board_t m_array{ };// Maybe the previous one was better?
+    //~ std::vector<char> array;
+};
+
+// Printing the board (Marco format)
+namespace MarcoPrint{
+void marcoPrint(std::ostream& stream, std::size_t sizeY, const board_t& array);
+void print_array_top_row(std::ostream& stream, std::size_t size);
+void print_array_bottom_row(std::ostream& stream, std::size_t size);
+void print_array_empty_row(std::ostream& stream, std::size_t size);
+void print_array_filled_row(std::ostream& stream, std::size_t pos, const std::vector<char>& row);
+void print_array_number_row(std::ostream& stream, std::size_t size);
+}// End namespace MarcoPrint
+
+// Printing the board (Joseph format)
+namespace JosephPrint{
+void josephPrint(std::ostream& stream, std::size_t sizeY, const board_t& array);
+void printHorizontalHead(std::ostream& stream, std::size_t size);
+void printHorizontalLine(std::ostream& stream, std::size_t size);
+void printHorizontalData(std::ostream& stream, std::size_t pos, const std::vector<char>& row);
+std::string getFormatedNumber(int number);
+}// End namespace JosephPrint
+
 }// End namespace FIAR
 
 #endif
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,4 +1,7 @@
 #include "Board.h"
+#include <string>
+#include <iomanip>
+
 
 // HALLO, ICH BIN EIN KOMMENTAR!!
 
@@ -92,6 +95,14 @@ std::vector<WinningSequence> Board::find_winning_sequences( void ){
     return found_sequences;
 }
 
+// Telling whether the board is valid
+bool Board::isValid() const{
+    return (m_sizeX > 0 && m_sizeY > 0);
+}
+// Returning the number of slots (the area) of the board
+std::size_t Board::area() const{
+    return m_sizeX * m_sizeY;
+}
 // Building the board, taking care of the dimensions
 void Board::buildBoard(){
     // Clearing the previous board
@@ -113,9 +124,112 @@ void Board::buildBoard(){
     // Building a new board
     // TODO: check whether this step can fail with std::vector
     // Eventually adding an upper limit to the dimensions
-    m_array = std::vector<std::vector<char>>(m_sizeX, std::vector<char>(m_sizeY, m_defaultSymbol));
+    m_array = board_t(m_sizeX, std::vector<char>(m_sizeY, m_defaultSymbol));
 }
 
+// Overloading the << insertion operator
+std::ostream& operator<<(std::ostream& stream, const Board& board){
+    // TODO: switching from one to the other could be a board option
+    // Both work, both are cool
+    //if(board.isValid()) MarcoPrint::marcoPrint(stream, board.m_sizeY, board.m_array);
+    if(board.isValid()) JosephPrint::josephPrint(stream, board.m_sizeY, board.m_array);
+    return stream;
+}
+
+// Printing the board (Marco format)
+namespace MarcoPrint{
+void marcoPrint(std::ostream& stream, std::size_t sizeY, const board_t& array){
+    stream << '\n' << '\n';
+    MarcoPrint::print_array_number_row( stream, sizeY );
+    MarcoPrint::print_array_top_row( stream, sizeY );
+    for (std::size_t i {0}; i < array.size(); ++i){
+        MarcoPrint::print_array_empty_row( stream, sizeY );
+        MarcoPrint::print_array_filled_row( stream, i+1, array[i] );
+        MarcoPrint::print_array_bottom_row( stream, sizeY );
+    }
+    stream << '\n' << '\n';
+    stream.flush();
+}
+void print_array_top_row(std::ostream& stream, std::size_t size){
+    stream << "    ";
+    for (std::size_t i {0}; i < size; ++i) stream << "____";
+    stream << "_" << '\n';
+}
+void print_array_bottom_row(std::ostream& stream, std::size_t size){
+    stream << "    ";
+    for (std::size_t i {0}; i < size; ++i) stream << "|___";
+    stream << "|" << '\n';
+}
+void print_array_empty_row(std::ostream& stream, std::size_t size){
+    stream << "    ";
+    for (std::size_t i {0}; i < size; ++i) stream << "|   ";
+    stream << "|" << '\n';
+}
+void print_array_filled_row(std::ostream& stream, std::size_t pos, const std::vector<char>& row){
+    int padlen_left;
+    int padlen_right;
+    std::string str{ std::to_string(pos) };
+    padlen_left = (4 + 1 - str.size()) / 2;
+    padlen_right = 4 - str.size() - padlen_left;
+    printf("%*s%s%*s", padlen_left, "", str.c_str(), padlen_right, "");
+    for (auto i_content: row) stream << "| " << i_content << " ";
+    stream << "|" << std::endl;
+}
+void print_array_number_row(std::ostream& stream, std::size_t size){
+    int padlen_left;
+    int padlen_right;
+    std::string i_str;
+    stream << R"( x\y)";
+    for (std::size_t i {0}; i < size; ++i){
+        i_str = std::to_string(i+1);
+        padlen_left = (3 + 1 - i_str.size()) / 2;
+        padlen_right = 3 - i_str.size() - padlen_left;
+        printf(" %*s%s%*s", padlen_left, "", i_str.c_str(), padlen_right, "");
+    }
+    stream << " \n";
+}
+}// End namespace MarcoPrint
+
+namespace JosephPrint{
+// Width taken by a column
+constexpr int g_colW{ 4 };
+void josephPrint(std::ostream& stream, std::size_t sizeY, const board_t& array){
+    stream << '\n' << '\n';
+    printHorizontalHead(stream, sizeY);
+    printHorizontalLine(stream, sizeY);
+    for (std::size_t i {0}; i < array.size(); ++i){
+        printHorizontalData(stream, i + 1, array[i]);
+        printHorizontalLine(stream, sizeY);
+    }
+    stream << '\n' << '\n';
+    stream.flush();
+}
+void printHorizontalHead(std::ostream& stream, std::size_t size){
+    stream << " x\\y";
+    for(std::size_t i{0}; i < size; ++i) stream << getFormatedNumber(i + 1);
+    stream << '\n';
+}
+void printHorizontalLine(std::ostream& stream, std::size_t size){
+    stream << "    ";
+    for (std::size_t i {0}; i < size; ++i) stream << "+---";
+    stream << "+\n";
+}
+void printHorizontalData(std::ostream& stream, std::size_t pos, const std::vector<char>& row){
+    stream << getFormatedNumber(pos);
+    for(char content: row) stream << "| " << content << ' ';
+    stream << "|\n";
+}
+std::string getFormatedNumber(int number){
+    static std::string convStr;
+    static std::string outStr;
+    static std::size_t margin;
+    outStr = "    ";
+    convStr = std::to_string(number);
+    margin = 1 + (g_colW - convStr.size()) / 2;
+    for(std::size_t j{margin}; j < outStr.size() && j - margin < convStr.size(); ++j)
+        outStr[j] = convStr[j - margin];
+    return outStr;
+}
+}// End namespace JosephPrint
+
 }// End namespace FIAR
-
-
